682a1a0233cb959b83e27871d36960cdb0d27bf3
adshidtadka <nextage900fx@gmail.com>
2020-05-30 12:33:05 +0900

    カスタムディレクティブ

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
+// Vue.directive("border", {
+  // bind(el, binding, vnode) {
+  //   // ディレクティブが初めて対象の要素に紐づいた時
+  // },
+  // inserted(el, binding, vnode) {
+  //   // 親Nodeに挿入された時
+  // },
+  // update(el, binding, vnode, oldVnode) {
+  //   // コンポーネントが更新された時, 子コンポーネントが更新される前
+  // },
+  // componentUpdated(el, binding, vnode, oldVnode) {
+  //   // コンポーネントが更新された時, 子コンポーネントが更新された後
+  // },
+  // unbind(el, binding, vnode) {
+  //   // ディレクティブが紐づいている要素から取り除かれた時
+  // },
+// })
 
 new Vue({
   render: h => h(App),
