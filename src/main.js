bcadd6a4c062d6923b4152208bd2c3d8505e409f
adshidtadka <nextage900fx@gmail.com>
2020-05-31 12:33:30 +0900

    reset

--- src/main.js
+++ src/main.js
@@ -1,36 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
-import LikeNumber from "./components/LikeNumber.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-Vue.component("LikeNumber", LikeNumber);
-// Vue.directive("border", {
-// bind(el, binding, vnode) {
-//   // ディレクティブが初めて対象の要素に紐づいた時
-// },
-// inserted(el, binding, vnode) {
-//   // 親Nodeに挿入された時
-// },
-// update(el, binding, vnode, oldVnode) {
-//   // コンポーネントが更新された時, 子コンポーネントが更新される前
-// },
-// componentUpdated(el, binding, vnode, oldVnode) {
-//   // コンポーネントが更新された時, 子コンポーネントが更新された後
-// },
-// unbind(el, binding, vnode) {
-//   // ディレクティブが紐づいている要素から取り除かれた時
-// },
-// })
-Vue.filter("upperCase", function(value) {
-  return value.toUpperCase();
-});
-
-Vue.mixin({
-  created() {
-    console.log("global mixin");
-  }
-});
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
