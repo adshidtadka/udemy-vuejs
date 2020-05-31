94c591df460fabed4a8514f9f3ebadb1a2f43a21
adshidtadka <nextage900fx@gmail.com>
2020-06-01 02:29:37 +0900

    vue router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+  if (to.path === "/users/1") {
+    next("/");
+  }
+  next();
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
